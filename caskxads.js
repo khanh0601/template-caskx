c12a37f83ac78342f47498d38f42a4f2881f56bd
khanh0601 <89721214+khanh0601@users.noreply.github.com>
2025-05-28 09:36:00 +0700

    update

---
+++ caskxads.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+    // if(textCir !== undefined){
+        let textCir = new CircleType('.tp-hero-title');
+    textCir.radius(900); 
+    textCir.dir(1.05);
+    // }
+}, 1000);
